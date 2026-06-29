d808a8fa8519c0caee51c43c44040aca6a47fda9
Joaquin <jolmedo@uade.edu.ar>
2026-06-29 01:26:25 -0300

    setup de favoritos

---
+++ app/(tabs)/favoritos.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { View,
+    Text,
+    ScrollView,
+    TouchableOpacity,
+    StyleSheet,
+    SafeAreaView,
+    Image,
+    }from 'react-native';
+import { colors } from '@/theme/colors';
+import { typography } from '@/theme/typography';
